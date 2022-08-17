584226dd1c41c878b81fb9df20fb0781dc4495db
Jordan <jbrockerrudow@gmail.com>
2022-08-17 17:28:00 -0500

    Change APIBases to const

--- src/app/shared/apiBaseUrls.ts
+++ src/app/shared/apiBaseUrls.ts
@@ -1,4 +1,4 @@
-class APIBase {
+export class APIBase {
     public base_url: string;
     public docs: string;
 
@@ -6,17 +6,9 @@ class APIBase {
         this.base_url = base_url;
         this.docs = docs;
     }
-}
-export class importIO {
-    base_url: string;
-    docs: 'http://api.docs.import.io/#rss.import.io';
 };
 
-export class tasteDrive {
-    base_url: string;
-    docs: string;
-};
-
-export class openLibrary {
-    base_url
-}
+export const tasteDrive = new APIBase('', '');
+export const gutenDex = new APIBase('https://gutendex.com/books', 'https://gutendex.com/');
+export const importIO = new APIBase('', 'http://api.docs.import.io/#rss.import.io');
+export const openLibrary = new APIBase('http://openlibrary.org/', 'https://openlibrary.org/developers/api');
