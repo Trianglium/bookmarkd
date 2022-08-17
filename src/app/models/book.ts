5412edf5f6fcb7ca02562f8cf2105dfa1dbbfb33
Jordan <jbrockerrudow@gmail.com>
2022-08-17 16:07:01 -0500

    Book and Author Models

---
+++ src/app/models/book.ts
@@ -0,0 +1,12 @@
+export class Author {
+    name: string;
+
+};
+export class Book {
+    title: string;
+    author: string;
+    year: string;
+    cover: string;
+    description: string;
+    genre: string;
+};
