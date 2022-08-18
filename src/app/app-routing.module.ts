6d9eb2fbfcd5d44b27d43575424181153ca3ce6a
Jordan <jbrockerrudow@gmail.com>
2022-08-18 13:50:07 -0500

    Remove routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'lists',
-    component: ListsComponent
-  },
-  {
-    path: 'lists/new',
-    component: ListAddComponent
-  },
-  {
-    path: 'lists/:slug/edit',
-    component: ListEditComponent
-  }
 ];
 
 @NgModule({
