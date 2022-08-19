371b23878186071f26ca19647bab32ffba080939
Jordan <jbrockerrudow@gmail.com>
2022-08-19 08:34:10 -0500

    Initialize Highlight directive

---
+++ src/app/directives/highlightht.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlighthtDirective } from './highlightht.directive';
+
+describe('HighlighthtDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlighthtDirective();
+    expect(directive).toBeTruthy();
+  });
+});
