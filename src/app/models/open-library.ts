8f54e66838ee37145acf391fadda40172d0dfd29
Jordan <jbrockerrudow@gmail.com>
2022-08-19 12:34:59 -0500

    Create BookCover and Search Result models

---
+++ src/app/models/open-library.ts
@@ -0,0 +1,10 @@
+export class SearchResult {
+   numFound: number | any;
+   start: number | any;
+   numFoundExact: boolean | any;
+   docs: any;
+};
+
+export class BookCover {
+   imageSrc: string;
+};
