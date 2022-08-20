20b65a25d3fdda1f5799b1413de6c850b40a8b2e
Jordan <jbrockerrudow@gmail.com>
2022-08-20 13:39:50 -0500

    Uodate fields on List model to allow null

--- src/app/models/list.ts
+++ src/app/models/list.ts
@@ -2,14 +2,14 @@ import { Book } from './book';
 import { Tag } from './tag';
 export class ListEntry {
     book: Book;
-    notes: string;
-    spoilers: boolean;
+    notes: string | null;
+    spoilers: boolean = false;
 };
 export class List {
     name: string;
-    description: string;
-    tags: Tag[];
-    public: boolean;
-    ranked: boolean;
+    description: string | null;
+    tags: Tag[] | null;
+    public_list: boolean = true;
+    ranked_list: boolean = false;
     entries: ListEntry[];
 };
