f22bc147cda73c4e79abd9475e4f99b6304f3a4f
Jordan <jbrockerrudow@gmail.com>
2022-08-20 13:39:14 -0500

    Create Subjects and WorksOfSubject models

---
+++ src/app/models/subject.ts
@@ -0,0 +1,25 @@
+export class Subject {
+    key: string;
+    name: string;
+    subject_type: string;
+    work_count: number;
+    works: number;
+    ebook_count: any;
+    subjects: any;
+    places: any;
+    people: any;
+    times: any;
+    authors: any;
+    publishers: any;
+    languages: any;
+    publishing_history: any;
+}
+
+export class WorksOfSubject {
+    key: string;
+    name: string;
+    subject_type: string;
+    work_count: any;
+    works: any;
+    ebook_count: any;
+}
