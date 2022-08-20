dc9f7e44ab01c3f446917021571d7b770ec459db
Jordan <jbrockerrudow@gmail.com>
2022-08-20 09:00:19 -0500

    Create Author and AuthorWorks models

---
+++ src/app/models/author.ts
@@ -0,0 +1,28 @@
+export class Author {
+    wikipedia: string;
+    source_records: any;
+    photos: any;
+    links: any;
+    alternate_names: any;
+    remote_ids: any;
+    name: string;
+    birth_date: string;
+    personal_name: string;
+    title: string;
+    type: any;
+    entity_type: string;
+    bio: string;
+    key: string;
+    latest_revision: any;
+    revision: any;
+    created: any;
+    last_modified: any;
+};
+
+
+
+export class AuthorWorks {
+    links: any;
+    size: any;
+    entries: any;
+}
