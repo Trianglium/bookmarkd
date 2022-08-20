45548df3b9fc2a5d866e788e8bc3482f17ce5cdb
Jordan <jbrockerrudow@gmail.com>
2022-08-20 13:41:24 -0500

    Mock List data added

--- src/app/models/lists.ts
+++ src/app/models/lists.ts
@@ -1,3 +1,36 @@
-export class List {
-    
-}
+import { List } from './list';
+export const LISTS: List[] = [
+    {
+        name: "Best 2010\'s Horror",
+        description: "",
+        tags: [
+            {
+                value: "horror"
+            },
+            {
+                value: "2010"
+            },
+            {
+                value: "scary"
+            }
+        ],
+        public_list: true,
+        ranked_list: true,
+        entries: [
+            {
+                bibkey: "",
+                title: "The Passage",
+                year: "2009",
+                authors: [
+                    {
+                        key: "OL896513A",
+                        name: "Justin Cronin"
+                        
+                    }
+                ],
+                data: null
+            },
+
+        ],
+    }
+];
