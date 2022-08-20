41b554273a8bcdefa20e594e507676d612f3be82
Jordan <jbrockerrudow@gmail.com>
2022-08-20 13:41:47 -0500

    Create ISBN and LCCN models for book data

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,10 +1,53 @@
+import { Author } from './author';
+export class ISBN {
+    url: string;
+    key: string;
+    title: string;
+    subtitle: string;
+    authors: any;
+    number_of_pages: number;
+    pagination: string;
+    by_statement: string;
+    identifiers: any;
+    classifications: any;
+    publishers: any;
+    publish_places: any;
+    publish_date: string;
+    subjects: any;
+    notes: string;
+    ebooks: any;
+    cover: any;
+};
+export class LCCN {
+    url: string;
+    key: string;
+    title: string;
+    subtitle: string;
+    authors: any;
+    number_of_pages: number;
+    pagination: string;
+    identifiers: any;
+    classifications: any;
+    publishers: any;
+    publish_places: any;
+    publish_date: string;
+    subjects: any;
+    cover: any;
+};
+
+export class BookData {
+    isbn: ISBN;
+    lccn: LCCN;
+
+}
+
 export class Book {
-    [key: string] {
+    bibkey?: any | null;
+    title: string;
+    year: string;
+    authors: Author[];
+    data?: BookData | null;
 
-    };
-    [key: string] {
-
-    };
 };
 
 export class BookCover {
